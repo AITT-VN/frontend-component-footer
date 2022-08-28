0efd0db37b4d9afc54bc0a23e84e430d4ac6ee46
Minh Ha <minhha@aitt.vn>
2022-08-28 16:23:06 +0700

    Update footer links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -65,8 +65,8 @@ class SiteFooter extends React.Component {
                 <div class="col-12 col-sm-8">
                     <div class="row">
                         <div class="col-12 col-sm-6 col-md-4">
-                            <p><a href="{config.LMS_BASE_URL}/courses">Các khóa học</a></p>
-                            <p><a href="{config.LMS_BASE_URL}/faq">Các câu hỏi thường gặp</a></p>
+                            <p><a href="${config.LMS_BASE_URL}/courses">Các khóa học</a></p>
+                            <p><a href="${config.LMS_BASE_URL}/faq">Các câu hỏi thường gặp</a></p>
                             <p><a href="https://discord.com/channels/962380027536764968/962380027972968450">Trung tâm trợ giúp</a></p>
                         </div>
                         <div class="col-12 col-sm-6 col-md-4">
@@ -82,7 +82,7 @@ class SiteFooter extends React.Component {
                 </div>
             </div>
 
-            <p class="copy-right text-center">© 2022: Bản quyền thuộc về OhStem Education và đã được bảo hộ</p>
+            <p class="copy-right text-center">© 2022 OhStem Education</p>
         </div>
         <hr class="hr-footer" />
     </div>
